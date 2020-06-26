cb0e7ed8c4e63d200f146e9e61f92eab1a958b78
Belén Rickmers <belen.rickmers@usach.cl>
2020-06-26 17:48:44 -0400

    Se crean seeder y faker

---
+++ database/seeds/UserProductTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
