56246f3fd505c57b74f9fb748de9751f5adf28ff
Belén Rickmers <belen.rickmers@usach.cl>
2020-09-06 22:42:42 -0300

    navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,9 +43,15 @@
             <div class="active-pink-3 active-pink-4 mb0.5 mx-4 col-lg-6">
                 <input class="form-control" type="text" placeholder="Buscar producto" aria-label="Search">
             </div>
-            <button class="btn btn-lg" style="background-color:transparent;">
-                <i class="fas fa-user"></i> Edit
-             </button>
+
+           
+            <button type="button" class="btn btn-outline-cuenta">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
+                Mi Cuenta
+            </button>
+            
         </div>
 
     </nav>
